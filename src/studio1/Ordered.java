6afcbc61adf9f4ae93e4389bdf8b83b13f20517f
tmantineo <e.l.mantineo@wustl.edu>
2025-09-03 14:00:25 -0500

    Mark did this

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,10 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		boolean isOrdered = x < y && y < z;
+		if (isOrdered) {
+			System.out.println(isOrdered);
+		}
 
 	}
 
